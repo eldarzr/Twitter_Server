80a9dc719081b6be177a27dee87ce3e95e026b6f
eldar <eldarzr@post.bgu.ac.il>
2022-01-07 00:08:32 +0200

    f

--- src/main/java/bgu/spl/net/impl/BGSServer/Notification.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/Notification.java
@@ -9,16 +9,16 @@ public class Notification implements Command<String> {
     public void execute(String args, int connectionId) {
         Manneger manneger = Manneger.getInstance();
         manneger.getConnections().send(connectionId,"9");
-        String noType = "" + args.charAt(0);
-        args = args.substring(1);
-        manneger.getConnections().send(connectionId,noType);
-        User user = manneger.getUser(connectionId);
-        if(user == null)
-            return;
-        String postingUser = user.getUserName();
-        manneger.getConnections().send(connectionId,postingUser + "\0");
-        manneger.getConnections().send(connectionId,args + "\0");
-        manneger.getConnections().send(connectionId,"NOTIFICATION;");
+//        String noType = "" + args.charAt(0);
+//        args = args.substring(1);
+//        manneger.getConnections().send(connectionId,noType);
+//        User user = manneger.getUser(connectionId);
+//        if(user == null)
+//            return;
+//        String postingUser = user.getUserName();
+//        manneger.getConnections().send(connectionId,postingUser + "\0");
+        manneger.getConnections().send(connectionId,args);
+        manneger.getConnections().send(connectionId,";");
 
     }
 }
