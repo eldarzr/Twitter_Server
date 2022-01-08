5f4a06f6e30812dcdf4df2eab98399095e214b1c
naor <naorq@post.bgu.ac.il>
2022-01-08 22:45:07 +0200

    asdfasdf

---
+++ src/main/java/bgu/spl/net/impl/BGSServer/PrivateMessage.java
@@ -0,0 +1,20 @@
+package bgu.spl.net.impl.BGSServer;
+
+import bgu.spl.net.api.bidi.Command;
+
+public class PrivateMessage implements Command<String> {
+    @Override
+    public void execute(String args, int connectionId) {
+        Manneger manneger = Manneger.getInstance();
+        String content = args;
+        String s = "6";
+        Command command;
+        if(manneger.sendPM(content,connectionId))
+            command = new ACK();
+        else
+            command = new Error();
+        command.execute(s,connectionId);
+
+
+    }
+}
